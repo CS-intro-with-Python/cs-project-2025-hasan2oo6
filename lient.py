0c9218fa65e9ab0cd6fd483114c4c2fc5a5eda22
Hasan Ghulomzoda <nozimzodahasan@gmail.com>
2025-11-14 12:48:55 +0200

    Create lient.py

---
+++ lient.py
@@ -0,0 +1,12 @@
+import requests
+import sys
+
+response = requests.get("http://localhost:5000/")
+
+print("Received:", response.text)
+
+if response.status_code != 200:
+    print("Server error")
+    sys.exit(1)   # FAIL workflow
+else:
+    print("Server OK")
